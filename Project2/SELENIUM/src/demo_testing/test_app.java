3f0af96c49d2c3c2448de9595dfe411f04f100c0
RamisaAnan <47281228+RamisaAnan@users.noreply.github.com>
2019-09-03 22:58:37 +0600

    upto name added in setinputfield function

--- Project2/SELENIUM/src/demo_testing/test_app.java
+++ Project2/SELENIUM/src/demo_testing/test_app.java
@@ -69,6 +69,55 @@ public class test_app {
 			Thread.sleep(1000);
 			driver.findElement(By.xpath("/html/body/div[18]/div/input")).sendKeys(Keys.RETURN);
 			
+			Thread.sleep(1000);
+			driver.findElement(By.xpath("//*[@id=\"tchkin\"]/div/input")).click();
+			Thread.sleep(1000);
+			
+			driver.findElements(By.cssSelector
+		            ("body > div:nth-child(16) > div.datepicker-days > table > tbody > tr:nth-child(5) > td:nth-child(5)")).get(0).click();
+			Thread.sleep(2000);
+
+//			driver.findElement(By.xpath("//*[@id=\"adults\"]/option[4]")).sendKeys(Keys.RETURN);
+			
+			Select select = new Select(driver.findElement(By.id("adults")));
+			Thread.sleep(1000);
+			select.selectByIndex(2);
+			Thread.sleep(1000);
+			
+			Select select2 = new Select(driver.findElement(By.id("tourtype")));
+			Thread.sleep(1000);
+			select2.selectByIndex(1);
+			Thread.sleep(2000);
+			
+			
+			driver.findElement(By.xpath("//*[@id=\"tours\"]/form/div[5]/button")).click();
+			Thread.sleep(3000);
+			
+			jse = (JavascriptExecutor)driver;
+			jse.executeScript("scroll(0,1200)");
+			Thread.sleep(2000);
+			
+			driver.findElement(By.xpath("//*[@id=\"body-section\"]/div[3]/div[2]/div[2]/div/div/form/div/div[2]/input")).click();
+			Thread.sleep(1000);
+		
+			driver.findElements(By.cssSelector
+		            ("body > div.datepicker.dropdown-menu > div.datepicker-days > table > tbody > tr:nth-child(5) > td:nth-child(4)")).get(0).click();
+			Thread.sleep(2000);
+			
+			driver.findElement(By.xpath("//*[@id=\"body-section\"]/div[3]/div[2]/div[2]/div/div/form/div/div[2]/button")).click();
+			Thread.sleep(3000);
+			
+			jse = (JavascriptExecutor)driver;
+			jse.executeScript("scroll(0,1400)");
+			Thread.sleep(2000);
+			
+			//driver.findElement(By.xpath("//*[@id=\"body-section\"]/div[3]/div[2]/div[2]/div/form/div[4]/button")).click();
+			//driver.findElement(By.linkText("Book Now")).click();
+			driver.findElement(By.cssSelector("#body-section > div:nth-child(7) > div.panel.panel-default > div.panel-body > div > form > div:nth-child(5) > button")).click();
+			Thread.sleep(3000);
+			//#body-section > div:nth-child(7) > div.panel.panel-default > div.panel-body > div > form > div:nth-child(5) > button
+
+			driver.findElement(By.xpath("//*[@id=\"guestform\"]/div[1]/div[2]/input")).sendKeys("Mousumi");			
 		}
 		
 	}
